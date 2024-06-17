c215a83f611a55a97ff89c41e9908fedec3b517c
amirabas <amirjabic@gmail.com>
2024-06-17 11:54:32 +0330

    create reduncer rt

--- src/Redux/store/todo.ts
+++ src/Redux/store/todo.ts
@@ -1,10 +1,31 @@
 console.log("create Action");
 
-const ADD_TODO = "CAKE_ORDERRED";
+const CAKE_ORDERRED = "CAKE_ORDERRED";
 
 function orderCake() {
     return {
-        type: ADD_TODO,
+        type: CAKE_ORDERRED,
         quantity: 1,
     };
 }
+
+// reducer
+
+// (previousState,action) => newState
+
+const initialState = {
+    numofCake: 10,
+    anotherProparty: 0,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERRED:
+            return {
+                ...state,
+                numofCake: state.numofCake - 1,
+            };
+        default:
+            return state;
+    }
+};
