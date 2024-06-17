e24e0e4f867220ff301db84e350faee8ad240ee4
amirabas <amirjabic@gmail.com>
2024-06-17 12:46:28 +0330

    create store unsubscribe and store.dispatch how work

--- src/Redux/store/todo.ts
+++ src/Redux/store/todo.ts
@@ -1,11 +1,19 @@
-console.log("create Action");
+import { createStore } from "redux";
 
 const CAKE_ORDERRED = "CAKE_ORDERRED";
+const CAKE_RESOCKED = "CAKE_RESOCKED";
 
 function orderCake() {
     return {
         type: CAKE_ORDERRED,
         quantity: 1,
+    };
+}
+
+function resockCake(qn = 1) {
+    return {
+        type: CAKE_RESOCKED,
+        quantity: qn,
     };
 }
 
@@ -25,7 +33,25 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numofCake: state.numofCake - 1,
             };
+        case CAKE_RESOCKED:
+            return {
+                ...state,
+                numberOfCake : state.numofCake + action.quantity,
+            };
         default:
             return state;
     }
 };
+
+const store = createStore(reducer);
+
+console.log('inital state' ,store.getState())
+
+const unsubscribe = store.subscribe(()=>{
+    console.log("Update state" ,store.getState())
+})   
+
+store.dispatch(resockCake(10))
+store.dispatch(orderCake())
+
+unsubscribe()
