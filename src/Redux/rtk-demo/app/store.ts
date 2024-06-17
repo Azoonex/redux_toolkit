cdb01b1ebc1eae556ff62a7994bbb82251493697
amirabas <amirjabic@gmail.com>
2024-06-17 20:52:52 +0330

    setup createLogger and set middleware in rtk

--- src/Redux/rtk-demo/app/store.ts
+++ src/Redux/rtk-demo/app/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamActions = require("../features/icecream/icecreamSlice");
+import { createLogger } from "redux-logger";
 
 const store = configureStore({
     reducer: {
         cakeReducer,
         icecreamActions
     },
+    middleware: (getdDefaultMiddleware) => getdDefaultMiddleware().concat(createLogger),
+    devTools: true,
+    
 });
 
 module.exports = store;
